16d9ee13256fa56eb97f7fa80a6929a292aa3d98
R Hari Narayan <harinarayan2002@gmail.com>
2024-05-10 16:21:50 +0530

    New design; Front almost prepared

--- ImageProcessingApp/App/urls.py
+++ ImageProcessingApp/App/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.mainView, name= "home"),
     path("upload/", views.upload, name= "upload"),
+    path("apply/", views.apply , name="apply"),
 ]
--- ImageProcessingApp/App/views.py
+++ ImageProcessingApp/App/views.py
@@ -4,13 +4,16 @@ import cv2
 import os
 import base64
 
+UPLOAD_FOLDER = 'uploads'
 
 # Create your views here.
 def mainView(request):
     return render(request, "index.html")
 
 def upload(request):
-    UPLOAD_FOLDER = 'uploads'
+    
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
     if request.method == 'POST':
         # Check if the form is valid
         if 'image' in request.FILES:
@@ -25,38 +28,29 @@ def upload(request):
             # Read the saved image using OpenCV
             uploaded_image = cv2.imread(image_path)
 
-            processing_method = request.POST.get('processing_method', 'grayscale')
-
-            # Apply selected image processing method
-            if processing_method == 'grayscale':
-                processed_image = ip.grayscale(uploaded_image)
-
-            elif processing_method == 'sharpen':
-                processed_image = ip.sharpen(uploaded_image)
-
-            elif processing_method == 'blur':
-                processed_image = ip.blur(uploaded_image)
-
-            # Encode the processed image as base64
-            retval, buffer = cv2.imencode('.jpg', processed_image)
-            processed_image_base64 = base64.b64encode(buffer).decode('utf-8')
-
             # Encode the original image as base64
             retval, buffer = cv2.imencode('.jpg', uploaded_image)
             original_image_base64 = base64.b64encode(buffer).decode('utf-8')
 
-            # Cleanup: Remove the uploaded image file from disk
-            os.remove(image_path)
             data = {
                 "original_image_base64":original_image_base64,
-                "processed_image_base64":processed_image_base64
             }
 
-            return render(request, 'index.html', data)
+            return render(request, 'editor.html', data)
         
         else:
-                return HttpResponse('No image uploaded.', status=400)
+                return render(request, 'index.html',{"Message": 'No image uploaded.'})
     else:
         # Render the form if the request is not a POST request
-        return render(request, 'upload_image.html')
+        return render(request, 'index.html')
 
+def apply(request):
+    image_path = os.path.join(UPLOAD_FOLDER, "image")
+    original_image = cv2.imread(image_path)
+    processed_image= ip.grayscale(original_image)
+    retval, buffer = cv2.imencode('.jpg', processed_image)
+    original_image_base64 = base64.b64encode(buffer).decode('utf-8')
+    data = {
+                "original_image_base64":original_image_base64,
+    }
+    return render(request, 'editor.html', data)
